a067c9fab4785ccc2ad9e2c445a18e0eaa31c753
Kassim Ouattara <kassimouattarakassim@Kassims-Air.lan>
2026-07-24 10:01:23 -0500

    Working on navbar

---
+++ components/global/Container.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+cn;
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("mx-auto max-w-6xl xl:max-w-7xl px-8", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
