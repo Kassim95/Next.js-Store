7de823490870b57aba77841b966127b1e651cc1a
Kassim Ouattara <kassimouattarakassim@Kassims-Air.lan>
2026-07-26 13:24:36 -0500

    featured products

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,5 +1,12 @@
-const FavoriteToggleButton = () => {
-  return <div>FavoriteToggleButton</div>;
+import { Button } from "../ui/button";
+import { FaHeart } from "react-icons/fa";
+
+const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  return (
+    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
 };
 
 export default FavoriteToggleButton;
