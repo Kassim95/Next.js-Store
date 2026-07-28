5bc7298563c3a5cc54efa19317666fec5de82be6
Kassim Ouattara <kassimouattarakassim@Kassims-Air.lan>
2026-07-28 12:37:56 -0500

    UserIcon componnent

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,16 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+import { LuUser } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
+
+const UserIcon = async () => {
+  const user = await currentUser();
+  const profileImg = user?.imageUrl;
+
+  if (profileImg) {
+    return (
+      <img src={profileImg} className="h-6 w-6 rounded-full object-cover" />
+    );
+  }
+  return <LuUser className="h-6 w-6 bg-primary rounded-full text-white" />;
 };
 
 export default UserIcon;
