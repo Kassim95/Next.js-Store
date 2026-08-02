c7936fd3aa1e2c8bce0396f03b6945b8e3306ea7
Kassim Ouattara <kassimouattarakassim@Kassims-Air.lan>
2026-08-02 11:54:56 -0500

    FavoriteToggleButton and FavoriteToggleForm Components

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,12 +1,13 @@
-import { Button } from "../ui/button";
-import { FaHeart } from "react-icons/fa";
+import { CardSignInButton } from "../form/Buttons";
+import { fetchFavoriteId } from "@/utils/actions";
+import FavoriteToggleForm from "./FavoriteToggleForm";
+import { auth } from "@clerk/nextjs/server";
 
-const FavoriteToggleButton = ({ productId }: { productId: string }) => {
-  return (
-    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
-      <FaHeart />
-    </Button>
-  );
+const FavoriteToggleButton = async ({ productId }: { productId: string }) => {
+  const { userId } = auth();
+  if (!userId) return <CardSignInButton />;
+  const favoriteId = await fetchFavoriteId({ productId });
+  return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />;
 };
 
 export default FavoriteToggleButton;
